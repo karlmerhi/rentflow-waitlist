735e3ed0961e5e9f2ac5c9aa0ebfa29a7de93e44
karlmerhi <karlmerhi99@gmail.com>
2024-12-26 02:21:13 -0500

    Adding Reddit Pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Head from 'next/head';
+
 // config
 import config from "@/config/general";
 // components
@@ -20,6 +22,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <script>
+          {'!function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent ? p.sendEvent.apply(p, arguments) : p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','a2_g6hys02n7r4r');rdt('track', 'PageVisit');'}
+        </script>
+      </Head>
       <body className={inter.className + " bg-gray"}>
         {children}
         <Analytics />
