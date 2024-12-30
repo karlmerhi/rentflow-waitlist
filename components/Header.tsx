532b8169c4dbf238a58734ef726e398eea63e1c4
karlmerhi <karlmerhi99@gmail.com>
2024-12-30 07:07:01 -0500

    added linkedin link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,38 +1,29 @@
-import Image from "next/image";
-import Link from "next/link";
-// config
-import config from "@/config/general";
+import React from 'react';
+import Image from 'next/image';
+import { FaLinkedin } from 'react-icons/fa';
 
 const Header = () => {
   return (
-    <header className="flex-col sm:flex-row flex justify-between items-start">
-      <Image src={"RentFlowLogo.svg"} width={180} height={60} alt={config.title} />
-      <nav>
-        <ul className="flex sm:mt-0 mt-4 items-center lg:gap-7 gap-3 font-medium text-base sm:text-lg">
-          {/* <li className="text-black">
-            <Link href={config.socials.twitter} passHref legacyBehavior>
-              <a
-                className="px-5 py-2 rounded"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Twitter
-              </a>
-            </Link>
-          </li> */}
-          {/* <li className="bg-activeButton text-white rounded py-2">
-            <Link href={config.subscribeForm} passHref legacyBehavior>
-              <a
-                className="px-5 py-2 rounded"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Register
-              </a>
-            </Link>
-          </li> */}
-        </ul>
-      </nav>
+    <header className="flex items-center justify-between p-4 xl:mb-10 bg-white shadow-md">
+      <div className="flex items-center">
+        <Image
+          src="/RentFlowLogo.svg"
+          alt="Company Logo"
+          width={200}
+          height={200}
+          className="mr-4"
+        />
+      </div>
+      <div className="flex items-center">
+        <a
+          href="https://www.linkedin.com/company/rentflow-live/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-700 mr-4 transform transition-transform duration-200 hover:scale-125"
+        >
+          <FaLinkedin size={34} />
+        </a>
+      </div>
     </header>
   );
 };
