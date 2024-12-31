80c82ccca800c66dd13c5be57a3589022d659e60
karlmerhi <karlmerhi99@gmail.com>
2024-12-30 23:05:09 -0500

    header mt-10

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { About, Hero, Header, Price, Faq, HowWorks, Footer, Form } from "@/compo
 
 export default function Home() {
   return (
-    <main className="lg:mt-20 lg:mx-20 lg:p-12 bg-white mt-4 mx-4 p-8 mb-4 rounded">
+    <main className="lg:mt-10 lg:mx-20 lg:p-12 bg-white mt-4 mx-4 p-8 mb-4 rounded">
       <Header />
       <Hero />
       <About />
